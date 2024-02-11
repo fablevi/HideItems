1baccff89aeefdf6b75ea25a1a2a7bf8fa22e8df
Fábián Levente <97455713+fablevi@users.noreply.github.com>
2024-02-11 10:50:18 +0100

    Update
    -button is added

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import Gio from 'gi://Gio';
 import St from 'gi://St';
 
@@ -8,10 +10,16 @@ export default class HideItems extends Extension {
     }
 
     enable() {
+        console.log("Hide Items Extension started...");
         this._indicator = null;
 
         this._showIcon = null;
         this._hideIcon = null;
+
+        //default visibility is true, but in the future i prepare it to state management
+        this._visibility = true;
+
+        this._createButton()
     }
 
     disable() {
@@ -19,9 +27,11 @@ export default class HideItems extends Extension {
 
         this._showIcon = null;
         this._hideIcon = null;
+
+        this._visibility = null;
     }
 
-    _createButton(){
+    _createButton() {
         //icon size
         this._iconSize = 16;
 
@@ -31,6 +41,12 @@ export default class HideItems extends Extension {
         const hideAdwIcon = Gio.icon_new_for_string(`${this.path}/icons/right-symbolic.svg`);
         this._hideIcon = this._createIcon(hideAdwIcon);
 
+        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
+        //this._indicator.connect('button-press-event', this._buttonClicked.bind(this));
+
+        this._setButtonIcon();
+
+        Main.panel.addToStatusArea(this.uuid, this._indicator, 2, "left");
     }
 
     _createIcon(icon) {
@@ -39,5 +55,21 @@ export default class HideItems extends Extension {
             style_class: 'system-status-icon',
             icon_size: this._iconSize
         });
+    }
+
+    _setButtonIcon() {
+        try {
+            if(this._indicator.first_child !== null){
+                this._indicator.remove_child(this._indicator.first_child)
+            }
+            if (this._visibility) {
+                this._indicator.add_child(this._showIcon)
+            } else {
+                this._indicator.add_child(this._hideIcon)
+            }
+        }catch (e){
+            console.log("HideItems error: ",e)
+        }
+        
     }
 }
